7fa8f2d05f1b756a60f5b8d6e88c84b8b671ed49
Ralph <gabrinao.ralph@gmail.com>
2019-03-31 13:24:29 -0700

    Transactions container

--- src/containers/Transactions/Transactions.js
+++ src/containers/Transactions/Transactions.js
@@ -2,35 +2,73 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import actions from './actions';
+import { TransactionsList } from './TransactionsList';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import styled from 'styled-components';
+import { getGroupedTransactions, makeSelectTransactionsData, getAccountsData } from './selectors';
+
+const Wrapper = styled.div`
+	height: 100%;
+	padding: 40px;
+`;
+
+const renderGridItem = ({ sm, renderComponent }) => {
+	return (
+		<Grid item sm={sm}>
+			<Paper square={false}>
+				{renderComponent()}
+			</Paper>
+		</Grid>
+	);
+}
 
 export class Transactions extends React.Component {
 	componentDidMount() {
+		this.props.fetchAccounts();
 		this.props.fetchTransactions();
 	}
 
 	render() {
 		const state = this.props.Transactions;
-		console.log(state.data);
+		console.log(state);
+		const transactions = state.filteredTransactions;
+		const transactionsList = { sm: 6, renderComponent: () => <TransactionsList transactions={transactions} /> };
 		return (
-			<div>{state.data ? state.data.transactions[0].transactionId : 'No data to show.'}</div>
+			<Wrapper>
+				<Grid container spacing={24} justify='center'>
+					{renderGridItem(transactionsList)}
+				</Grid>
+			</Wrapper>
 		);
 	}
 }
 
 Transactions.propTypes = {
-	Transactions: PropTypes.any
+	Transactions: PropTypes.shape({
+		accountsData: PropTypes.object,
+		transactionsData: PropTypes.object,
+		filteredTransactions: PropTypes.array,
+		fetchAccounts: PropTypes.func,
+		fetchTransactions: PropTypes.func
+	})
 };
 
 /* istanbul ignore next */
 const mapStateToProps = state => {
 	return {
-		Transactions: state.Transactions
+		Transactions: {
+			accountsData: getAccountsData(state),
+			transactionsData: makeSelectTransactionsData(state),
+			filteredTransactions: getGroupedTransactions(state)
+		}
 	};
 };
 
 
 /* istanbul ignore next */
 const mapDispatchToProps = dispatch => ({
+	fetchAccounts: () => dispatch(actions.fetchAccounts()),
 	fetchTransactions: () => dispatch(actions.fetchTransactions())
 });
 
