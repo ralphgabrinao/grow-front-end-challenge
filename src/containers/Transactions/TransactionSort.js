d2a0dbcc7ef51eeb1701dc8da0710c6ba0fd4998
Ralph <gabrinao.ralph@gmail.com>
2019-04-02 15:10:01 -0700

    Sort by transaction date

--- src/containers/Transactions/TransactionSort.js
+++ src/containers/Transactions/TransactionSort.js
@@ -3,24 +3,22 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import styled from 'styled-components';
-import { allAccounts } from './constants';
 
 const Wrapper = styled.div`
-	height: 50px;
+	height: 25px;
 `;
 
-export const AccountFilter = (props) => {
+export const TransactionSort = (props) => {
 	const { options, value, handleChange } = props;
-	if (!value) return (<div></div>);
-	if (options && !options.find(a => a.accountId === '')) options.unshift(allAccounts);
-	const renderOptions = options ? options.map(a => <MenuItem key={a.accountId} value={a}>{a.accountName}</MenuItem>) : [];
+	if (!value) return ('');
+	const renderOptions = options ? options.map(a => <MenuItem key={a.key} value={a}>{a.label}</MenuItem>) : [];
 	return (
 		<Wrapper>
 			<FormControl>
 				<Select
 					value={value}
 					onChange={handleChange()}
-					name='account'>
+					name='sort'>
 					{renderOptions}
 				</Select>
 			</FormControl>
--- src/containers/Transactions/selectors.js
+++ src/containers/Transactions/selectors.js
@@ -4,6 +4,8 @@ import { createSelector } from 'reselect';
 export const getAccountsData = state => state.Transactions.accountsData;
 export const getCategories = state => state.Transactions.categories;
 export const getFilters = state => state.Transactions.filters;
+export const getSortOptions = state => state.Transactions.sortOptions;
+export const getSortValue = state => state.Transactions.sortValue;
 const getTransactionsData = state => state.Transactions.transactionsData;
 
 export const makeSelectTransactionsData = createSelector(
@@ -12,8 +14,12 @@ export const makeSelectTransactionsData = createSelector(
 		if (!transactionsData) return null;
 		const accounts = accountsData ? accountsData.accounts : [];
 		const transactions = transactionsData.transactions.map(t => {
+			const split = t.transactionDate.split('-');
+			const date = new Date(split[0], split[1], split[2]);
+			console.log(date);
 			return {
 				...t,
+				transactionDateTime: date,
 				category: t.category ? t.category : 'MISC',
 				account: accounts ? accounts.filter(a => a.accountId === t.accountId)[0] : null
 			};
@@ -27,20 +33,28 @@ const getFilteredTransactionsData = createSelector(
 	// 		accountsData changes, or
 	// 		makeSelectTransactionsData alters transactionsData (by adding account to each transaction), or
 	// 		the filters change
-	[getAccountsData, makeSelectTransactionsData, getFilters],
-	(accountsData, transactionsData, filters) => {
+	[getAccountsData, makeSelectTransactionsData, getFilters, getSortValue],
+	(accountsData, transactionsData, filters, sortValue) => {
 		if (!transactionsData) return null;
 		const transactions = transactionsData.transactions
 			.filter(t => 
 				filters.account && (!filters.account.accountId || filters.account.accountId === t.account.accountId) &&
 				filters.category && filters.category[t.category]);
+		return transactions;
+	}
+);
 
+const getSortedTransactiionsData = createSelector(
+	[getFilteredTransactionsData, getSortValue],
+	(transactions, sortValue) => {		
+		if (!transactions || !sortValue) return transactions;
+		transactions.sort(sortValue.comparer);
 		return transactions;
 	}
 );
 
 export const getGroupedTransactions = createSelector(
-	getFilteredTransactionsData,
+	getSortedTransactiionsData,
 	transactions => {
 		if (!transactions) return [];
 		const transactionDates = Array.from(new Set(transactions.map(t => t.transactionDate)));
